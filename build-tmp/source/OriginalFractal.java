ba5f1892a73a2a9f7e55333d96d46aec8dd8aa6e
unknown <jihuang65@697-334-Gothmog.sf.k12.internal>
2015-01-23 10:45:07 -0800

    started on fractals

--- build-tmp/source/OriginalFractal.java
+++ build-tmp/source/OriginalFractal.java
@@ -1,3 +1,19 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class OriginalFractal extends PApplet {
+
 public void setup()
 {
 	size(500,500);
@@ -23,4 +39,13 @@ public void inscribedSquares(double x, double y, double sideL, double minL)
 		inscribedSquares(x,y,sideL,sideL);
 		rotate(-(rad+(float)(Math.PI/4)));
 	}
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "OriginalFractal" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
