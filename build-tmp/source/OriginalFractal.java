26db728a27ff361e3958588feb7416de6b5c49a0
Jingbin Huang <ben441318936@gmail.com>
2015-01-24 20:46:59 -0800

    finished fractal
    add color shifting

--- build-tmp/source/OriginalFractal.java
+++ build-tmp/source/OriginalFractal.java
@@ -14,30 +14,42 @@ import java.io.IOException;
 
 public class OriginalFractal extends PApplet {
 
+double rad=(float)(Math.PI/4);
+double screenL=500;
+double sideLength=screenL*(Math.sqrt(2)/2);
+double numSquares=100;
 public void setup()
 {
-	size(500,500);
+	size((int)screenL,(int)screenL);
 	background(0);
-	translate(width/2,height/2); //orgin of the the coordinate system for the entire program is at center of screen
+	translate(width/2,height/2);
 	rectMode(RADIUS); //all squares are drawn with specified center and radius
-	stroke(0);
-	inscribedSquares(0,0,400,10);
+	strokeWeight(5);
+	inscribedSquares(0,0,sideLength,numSquares);
 }
 public void draw()
 {
 
 }
-public void inscribedSquares(double x, double y, double sideL, double minL)
+public void inscribedSquares(double x, double y, double sideL, double nSqures)
 {
-	float rad=(float)(Math.PI/4);
-	if(sideL<=minL) {rect((float)x,(float)y,(float)sideL/2,(float)sideL/2);}
-	else
+	if(nSqures>0)
 	{
-		rad=rad+(float)(Math.PI/4);
-		rotate(rad);
+		rotate((float)rad);
+		int r=(int)(Math.random()*256);
+		int g=(int)(Math.random()*256);
+		int b=(int)(Math.random()*256);
+		fill(r,g,b,127);
+		stroke(r,g,b);
+		rect((float)x,(float)y,(float)sideL/2,(float)sideL/2);
+		rotate((float)rad);
+		nSqures--;
+	}
+	if(nSqures>0) 
+	{
+		rad=rad+(double)(Math.PI/4);
 		sideL=sideL*(Math.sqrt(2)/2);
-		inscribedSquares(x,y,sideL,sideL);
-		rotate(-(rad+(float)(Math.PI/4)));
+		inscribedSquares(x,y,sideL,nSqures);
 	}
 }
   static public void main(String[] passedArgs) {
